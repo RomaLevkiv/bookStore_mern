b5289ffa172464918fd0d8d6048c166201c834e3
unknown <kvintaslev@i.ua>
2020-07-15 10:38:51 +0300

    init

---
+++ client/build/precache-manifest.9e077f1429eac785fc83046cea894da1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0a3edc5079131dc56a24af172945a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09a9e2e2a33763e5bed2",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "f91672148f070b11f618",
+    "url": "/static/css/main.bad19d85.chunk.css"
+  },
+  {
+    "revision": "09a9e2e2a33763e5bed2",
+    "url": "/static/js/2.68c4d18a.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.68c4d18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91672148f070b11f618",
+    "url": "/static/js/main.347fcf10.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "a5f0bf8f22b08ecdc3bd38bfb5dc12aa",
+    "url": "/static/media/book-shop.a5f0bf8f.png"
+  },
+  {
+    "revision": "fcf28b87255c74da2aa737159063fcbd",
+    "url": "/static/media/book.fcf28b87.png"
+  },
+  {
+    "revision": "16a5fbb5e2db4ebb670813ee595a1c0b",
+    "url": "/static/media/border.16a5fbb5.png"
+  }
+]);
